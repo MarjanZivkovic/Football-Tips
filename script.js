c2acd9628740be83c832a1260469c55801b5a504
MarjanZivkovic <zivkovicmarjan@yahoo.com>
2022-11-25 17:32:45 +0100

    loader added, logic finalized, fetching limited

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const searchGames = document.querySelector('.search-games');
+const loader = document.querySelector('.loader-container');
 const gamesContainer = document.querySelector('.games-container');
 const scoreSpan = document.querySelector('.score-tip');
 const goalSpan = document.querySelector('.goal-tip');
@@ -11,52 +12,70 @@ const year = date.getFullYear();
 //set current year, footer
 document.querySelector('.current-year').textContent = year;
 
-//fetching todays fixtures
-fetch(
-	//  https://v3.football.api-sports.io/fixtures?live="1-2-3-39-40-45-61-78-88-94-106-110-135-140-144-179-197-203-207-210-218-238-244-271-283-286-315-323-332-333-345-373-407"&last=1`, 
-	`https://v3.football.api-sports.io/fixtures?date=${year}-${month}-${day}`, 
-{
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "v3.football.api-sports.io",
-		"x-rapidapi-key": "e80663b34efa21c93f4b9507cc1d5aba"
-	}
-})
-.then(response => response.json())
-.then (data => data.response.forEach( el => {
-	const game = document.createElement('div');
-	game.classList.add('game');
-	game.innerHTML = `
-		<div class='game-container'>
-			<div class='status'>${el.fixture.status.short}</div>
-			<div class='teams'>
-				<div class='league'><small>${el.league.name}, ${el.league.country}</small></div>
-				<div> <div> <img src='${ el.teams.home.logo}'> ${el.teams.home.name} </div><div> <strong> ${el.goals.home === null ? '-' : el.goals.home}</strong></div></div> 
-				<div> <div><img src='${ el.teams.away.logo}'> ${el.teams.away.name} </div><div> <strong> ${el.goals.away === null ? '-' : el.goals.away}</strong> </div></div>
-			</div>
-			<button onclick='getTip(${el.teams.home.id}, ${el.teams.away.id})'>Our Tip</button>
-		</div>			
-		`;
-	gamesContainer.appendChild(game);
-})).then( () =>{
-	//filter leagues
-	const leagues = document.querySelectorAll('.league');
+//loading
+function loading(){
+	gamesContainer.hidden = true;
+}
 
-	function filterLeagues( searchedLeague ){
-		leagues.forEach( league => {
-			if( league.textContent.toLowerCase().includes( searchedLeague.toLowerCase() ) ){
-				league.parentNode.parentNode.parentNode.classList.remove('none');
-			} else {
-				league.parentNode.parentNode.parentNode.classList.add('none');
-			}
-		} )
-	}
+function loaded(){
+	loader.classList.add('none');
+	gamesContainer.hidden = false;
+}
 
-	searchGames.addEventListener('input', (e) => filterLeagues(e.target.value));
-})
-.catch(err => {
-	console.log(err);
-});
+//fetching todays fixtures on page loading
+function fetchFixtures(){
+	loading();
+	fetch(
+		//  `https://v3.football.api-sports.io/fixtures?live="1-2-3-39-40-45-61-78-88-94-106-110-135-140-144-179-197-203-207-210-218-238-244-271-283-286-315-323-332-333-345-373-407"&last=1`, 
+		`https://v3.football.api-sports.io/fixtures?date=${year}-${month}-${day}`, 
+	{
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "v3.football.api-sports.io",
+			"x-rapidapi-key": "e80663b34efa21c93f4b9507cc1d5aba"
+		}
+	})
+	.then(response => response.json())
+	.then (data => data.response.forEach( el => {
+				if ( el.fixture.status.short === 'NS' ||  el.fixture.status.short === 'TBD' || el.fixture.status.short === '1H' || el.fixture.status.short === 'HT' || el.fixture.status.short === '2H' || el.fixture.status.short === 'ET' || el.fixture.status.short === 'P' || el.fixture.status.short === 'INT'){
+					const game = document.createElement('div');
+					game.classList.add('game');
+					game.innerHTML = `
+						<div class='game-container'>
+							<div class='status'>${el.fixture.status.short}</div>
+							<div class='teams'>
+								<div class='league' id='league'><small>${el.league.name}, ${el.league.country}</small> <img src='${el.league.logo}' alt='League logo'></div>
+								<div> <div> <img src='${ el.teams.home.logo}' alt='Home team logo'> ${el.teams.home.name} </div><div> <strong> ${el.goals.home === null ? '-' : el.goals.home}</strong></div></div> 
+								<div> <div><img src='${ el.teams.away.logo}' alt='Away team logo'> ${el.teams.away.name} </div><div> <strong> ${el.goals.away === null ? '-' : el.goals.away}</strong> </div></div>
+							</div>
+							<button onclick='getTip(${el.teams.home.id}, ${el.teams.away.id})'>Our Tips</button>
+						</div>			
+						`;
+					gamesContainer.appendChild(game);
+		}
+	})).then( () =>{ loaded() })
+	.then( () =>{
+		//filter leagues
+		const leagues = document.querySelectorAll('.league');
+	
+		function filterLeagues( searchedLeague ){
+			leagues.forEach( league => {
+				if( league.textContent.toLowerCase().includes( searchedLeague.toLowerCase() ) ){
+					league.parentNode.parentNode.parentNode.classList.remove('none');
+				} else {
+					league.parentNode.parentNode.parentNode.classList.add('none');
+				}
+			} )
+		}
+	
+		searchGames.addEventListener('input', (e) => filterLeagues(e.target.value));
+	})
+	.catch(err => {
+		console.log(err);
+	});
+}
+
+fetchFixtures();
 		
 //head to head tips
 function getTip( home, away ){
@@ -64,6 +83,9 @@ function getTip( home, away ){
 	const goalArr = [];
 	let scoreTip;
 	let goalTip;
+
+	scoreSpan.classList.add('calculating');
+	goalSpan.classList.add('calculating');
 
 	fetch(`https://v3.football.api-sports.io/fixtures/headtohead?h2h=${home}-${away}&last=10`, {
 	"method": "GET",
@@ -92,11 +114,11 @@ function getTip( home, away ){
 		console.log(scoreArr, goalArr)
 		
 		if( scoreArr.length >= 5 ){
-			if ( (scoreArr.filter( x => x === '1').length) > ( (scoreArr.filter( x => x === 'x').length) + (scoreArr.filter( x => x === '2').length) ) ){
+			if ( ((scoreArr.filter( x => x === '1').length) > ( (scoreArr.filter( x => x === 'x').length) + (scoreArr.filter( x => x === '2').length) )) && (scoreArr.indexOf('1') === 0) ){
 				scoreTip = '1';
-			} else if ( (scoreArr.filter( x => x === '2').length) > ( (scoreArr.filter( x => x === 'x').length) + (scoreArr.filter( x => x === '1').length) ) ){
+			} else if ( ((scoreArr.filter( x => x === '2').length) > ( (scoreArr.filter( x => x === 'x').length) + (scoreArr.filter( x => x === '1').length) )) && (scoreArr.indexOf('2') === 0) ){
 				scoreTip = '2';
-			} else if ( (scoreArr.filter( x => x === 'x').length) > ( (scoreArr.filter( x => x === '1').length) + (scoreArr.filter( x => x === '2').length) ) ){
+			} else if ( ((scoreArr.filter( x => x === 'x').length) > ( (scoreArr.filter( x => x === '1').length) + (scoreArr.filter( x => x === '2').length) )) && (scoreArr.indexOf('x') === 0) ){
 				scoreTip = 'X';
 			} else {
 				scoreTip = '-';
@@ -106,15 +128,18 @@ function getTip( home, away ){
 		}
 
 	
-		if ( goalArr.filter( x => x === '3+').length > 5 ){
+		if ( (goalArr.filter( x => x === '3+').length > 5) && ( goalArr.indexOf('3+') === 0 ) ){
 			goalTip = '3+';
-		} else if ( goalArr.filter( x => x === '0-2').length > 5){
+		} else if ( (goalArr.filter( x => x === '0-2').length > 5) && ( goalArr.indexOf('0-2') === 0 ) ){
 			goalTip = '0-2';
 		} else {
 			goalTip = '-';
 		}
 		scoreSpan.textContent = scoreTip;
 		goalSpan.textContent = goalTip;
+
+		scoreSpan.classList.remove('calculating');
+		goalSpan.classList.remove('calculating');
 	})		
 	.catch(err => {
 		console.log(err);
